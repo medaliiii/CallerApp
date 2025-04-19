fea9267e1bf231eb9f9f5acde8b320930e38bbe3
dali <mabroukdali1925@gmail.com>
2025-04-19 20:34:00 +0200

    feat(db): add DatabaseHelper to manage SQLite database for user authentication

---
+++ app/src/main/java/com/example/callerapp/DatabaseHelper.java
@@ -0,0 +1,40 @@
+package com.example.callerapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "UsersDB.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public static final String TABLE_USERS = "users";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_PASSWORD = "password";
+    public static final String COLUMN_PHONE = "phone";
+
+    private static final String TABLE_CREATE =
+            "CREATE TABLE " + TABLE_USERS + " (" +
+                    COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_NAME + " TEXT NOT NULL, " +
+                    COLUMN_PASSWORD + " TEXT NOT NULL, " +
+                    COLUMN_PHONE + " TEXT NOT NULL);";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
+        onCreate(db);
+    }
+}
+
