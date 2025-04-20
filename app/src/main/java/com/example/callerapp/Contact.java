d4d2013397317d49c5a873f50170a789015b504f
dali <mabroukdali1925@gmail.com>
2025-04-20 18:06:49 +0200

    feat(model): add avatarUrl field to Contact class for avatar support

--- app/src/main/java/com/example/callerapp/Contact.java
+++ app/src/main/java/com/example/callerapp/Contact.java
@@ -1,62 +1,70 @@
-package com.example.callerapp;
+    package com.example.callerapp;
 
-public class Contact {
-    private int id;
-    private String nom;
-    private String pseudo;
-    private String num;
+    public class Contact {
+        private int id;
+        private String nom;
+        private String pseudo;
+        private String num;
+        private String avatarUrl;
 
-    public Contact(int id, String nom, String pseudo, String num) {
-        this.id = id;
-        this.nom = nom;
-        this.pseudo = pseudo;
-        this.num = num;
+        public Contact(int id, String nom, String pseudo, String num,String avatarUrl) {
+            this.id = id;
+            this.nom = nom;
+            this.pseudo = pseudo;
+            this.num = num;
+            this.avatarUrl = avatarUrl;
+        }
+
+        public Contact(String nom, String pseudo, String num) {
+            this.nom = nom;
+            this.pseudo = pseudo;
+            this.num = num;
+        }
+        public String getAvatarUrl() {
+            return avatarUrl;
+        }
+        public void setAvatarUrl(String avatarUrl) {
+            this.avatarUrl = avatarUrl;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getNom() {
+            return nom;
+        }
+
+        public void setNom(String nom) {
+            this.nom = nom;
+        }
+
+        public String getPseudo() {
+            return pseudo;
+        }
+
+        public void setPseudo(String pseudo) {
+            this.pseudo = pseudo;
+        }
+
+        public String getNum() {
+            return num;
+        }
+
+        public void setNum(String num) {
+            this.num = num;
+        }
+
+        @Override
+        public String toString() {
+            return "Contact{" +
+                    ", nom='" + nom + '\'' +
+                    ", pseudo='" + pseudo + '\'' +
+                    ", num='" + num + '\'' +
+                    '}';
+        }
     }
-
-    public Contact(String nom, String pseudo, String num) {
-        this.nom = nom;
-        this.pseudo = pseudo;
-        this.num = num;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPseudo() {
-        return pseudo;
-    }
-
-    public void setPseudo(String pseudo) {
-        this.pseudo = pseudo;
-    }
-
-    public String getNum() {
-        return num;
-    }
-
-    public void setNum(String num) {
-        this.num = num;
-    }
-
-    @Override
-    public String toString() {
-        return "Contact{" +
-                ", nom='" + nom + '\'' +
-                ", pseudo='" + pseudo + '\'' +
-                ", num='" + num + '\'' +
-                '}';
-    }
-}
